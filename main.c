efe0bb09442adc32009984d2139055afa73685d1
21stars <21200610@handong.edu>
2017-12-18 19:35:45 +0900

    Update main.c
    fixed except motorcontrol

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
  * Copyright (C) 2016 Ruslan V. Uss <unclerus@gmail.com>
  * BSD Licensed as described in the file LICENSE
  */
- 
 /*
  * Softuart example
  *
@@ -29,58 +28,74 @@
 #include "softuart/softuart.h"
 #include "pwm.h"
 
-#define RX_PIN 3
-#define TX_PIN 1
+#define RX_PIN 3 //D9
+#define TX_PIN 1 //D10
 
 #define TRIGGER_PIN 14	//D5
 #define ECHO_PIN    12	//D6
 
 #define MAX_DISTANCE_CM 500 // 5m max
+#define MAX_PWM_DUTY 5   // %
 
-int32_t distance = 0;
+#define A_PLUS 0	// A+ D3
+#define A_MINUS 5	// A- D1
+#define B_PLUS 2	// B+ D2
+#define B_MINUS 4	// B- D4
 
-void task1(void *pvParameters)
+int32_t distance = 9999;
+char key = 'p';
+char string_out[10];
+
+void bluetooth(void *pvParameters)
 {
     QueueHandle_t *queue = (QueueHandle_t *)pvParameters;
-    // setup software uart to 9600 8n1
+    // Baudrate 9600
     softuart_open(0, 9600, RX_PIN, TX_PIN);
     uint32_t count = 0;
     while (true)
     {
         if (!softuart_available(0))
             continue;
-
-        char c = softuart_read(0);
-	
-	if(c=='x') softuart_put(0,distance);
-	else softuart_put(0, c);
-
+        key = softuart_read(0);
+	//used to know distance	
+	if(key=='x')
+	{
+	  if(distance < 15) softuart_put(0,'a');
+	  else if((15 <= distance) && (distance < 30)) softuart_put(0,'b');
+	  else if((30 <= distance) && (distance < 45)) softuart_put(0,'c');
+	  else if((45 <= distance) && (distance < 60)) softuart_put(0,'d');
+	  if(60<=distance) softuart_put(0,'b');
+	}
+	else softuart_put(key);
         vTaskDelay(100);
         xQueueSend(*queue, &count, 0);
         count++;
     }
 }
 
-void task2(void *pvParameters)
+void ultrasonic(void *pvParameters)
 {
     QueueHandle_t *queue = (QueueHandle_t *)pvParameters;
-
+    //Trigger = GPIO14 = D5 // ECHO_PIN = 12 = D6
     ultrasonic_sensor_t sensor = {
         .trigger_pin = TRIGGER_PIN,
         .echo_pin = ECHO_PIN
     };
-
+    //GPIO_Enable
     ultrasoinc_init(&sensor);
 
     uint32_t count = 0;
-
     while(1) {
+	//  l = (v / t) / 2
 	uint32_t distance_temp = ultrasoinc_measure_cm(&sensor, MAX_DISTANCE_CM);
-        if (distance_temp < 0)
+	//receive error(no signal or farther than MAX_DISTANCE)        
+	if (distance_temp < 0)
 	    break;
-        else distance = distance_temp;
-	  
-//            printf("Distance: %d cm, %.02f m\n", distance, distance / 100.0);
+        else
+	{
+	  distance = distance_temp;
+//	  printf("dist: %d\n", distance_temp);
+	}
         vTaskDelay(100);
         xQueueSend(*queue, &count, 0);
         count++;
@@ -88,6 +103,65 @@ void task2(void *pvParameters)
 
 }
 
+
+
+void motorcontrol(void *pvParameters)
+{
+    QueueHandle_t *queue = (QueueHandle_t *)pvParameters;
+    uint32_t count = 0;
+    
+    uint8_t pins[2];
+    pins[0] = A_MINUS;	// A-
+    pins[1] = B_MINUS;	// B-
+    pwm_init(2, pins, true);  //enable pwm pin A-, B-
+    //PWM_FREQ
+    pwm_set_freq(1000);
+    //MAX POWER = (MAX_PWM_DUTY)%
+    pwm_set_duty(UINT16_MAX * MAX_PWM_DUTY/(double)100);
+    pwm_start();
+    gpio_enable(A_PLUS, GPIO_OUTPUT);
+    gpio_enable(B_PLUS, GPIO_OUTPUT);
+    
+    while(1) {
+	if(distance < 30) key ='p';
+	switch(key){
+	  //forward
+	  case 'w': case 'W':
+	    pwm_init(2,pins,true);
+	    gpio_write(A_PLUS,1);
+	    gpio_write(B_PLUS,1);
+	    break;
+	  //backward
+	  case 's': case 'S':
+	    gpio_write(A_PLUS,0);
+	    gpio_write(B_PLUS,0);
+	    break;
+	  //left
+	  case 'a': case 'A':
+	    pwm_init(2,pins,true);
+	    gpio_write(A_PLUS,1);
+	    gpio_write(B_PLUS,0);
+	    break;
+	  //right
+	  case 'd': case 'D':
+	    pwm_init(2,pins,true);
+	    gpio_write(A_PLUS,0);
+	    gpio_write(B_PLUS,0);
+	    break;
+	  //stop
+	  case 'p': case 'P':
+	    gpio_write(A_MINUS,0);
+	    gpio_write(B_MINUS,0);
+	    break;
+	  //else: keep
+	  default:
+	    break;
+	}
+        vTaskDelay(200);
+        xQueueSend(*queue, &count, 0);
+        count++;
+    }
+}
 
 static QueueHandle_t mainqueue;
 
@@ -96,8 +170,9 @@ void user_init(void)
     uart_set_baud(0, 115200);
     printf("SDK version:%s\n", sdk_system_get_sdk_version());
     mainqueue = xQueueCreate(10, sizeof(uint32_t));
-    xTaskCreate(task1, "tsk1", 256, &mainqueue, 2, NULL);
-    xTaskCreate(task2, "tsk2", 256, &mainqueue, 3, NULL);
+    xTaskCreate(bluetooth, "bluetooth", 256, &mainqueue, 2, NULL);
+    xTaskCreate(ultrasonic, "ultrasonic", 256, &mainqueue, 2, NULL);
+    xTaskCreate(motorcontrol, "motorcontrol", 256, &mainqueue, 2, NULL);
 }
 
 
