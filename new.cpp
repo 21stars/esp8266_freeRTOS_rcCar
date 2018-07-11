c06c4dbf3d888b2293101f396fdb137690f48e40
21stars <darkelode@gmail.com>
2018-07-11 09:33:43 +0900

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -15,11 +15,12 @@ int main(int, char**)
 	if (!cap.isOpened())  // check if we succeeded
 		return -1;
 
-	namedWindow("edges", 1);
 	for (;;)
 	{
 		Mat frame, gray, edges, ROI, white, yellow, mask;
 		vector<Vec4i> lines;
+		Vec4i lline, rline;
+		double langle = 180, rangle = 0;
 
 		cap >> frame; // get a new frame from camera
 
@@ -27,26 +28,42 @@ int main(int, char**)
 		inRange(frame, Scalar(0, 150, 150), Scalar(150, 200, 255), yellow);
 		mask = white + yellow;
 
-		imshow("white", white);
-		imshow("mask", mask);
+//		imshow("white", white);
+//		imshow("mask", mask);
 
-		//img smoothing
+//	img smoothing
 		GaussianBlur(mask, mask, Size(7, 7), 1.5, 1.5);
-		//Canny
+//	Canny
 		Canny(mask,edges, 0, 30, 3);
-		//HoughLine
+//	HoughLine
 		HoughLinesP(mask, lines, 1, CV_PI / 180, 50, 50, 50);
 
-		//Angle Filtering
+//	Angle Filtering
 		for (int i = 0; i < lines.size(); i++) {
 			Point A(lines[i][0], lines[i][1]), B(lines[i][2], lines[i][3]);
-			Vec4i l = lines[i];
+			//get angle
 			double angle = atan2((A.y - B.y), (A.x - B.x)) * 180 / CV_PI;
-			if (((angle < -30) && (angle > -60)) || ((angle < 150) && (angle > 120)) || ((angle > 30) && (angle < 60)) || ((angle > -150) && (angle < -120)))
-				line(frame, A, B, Scalar(0, 0, 255), 3, 8);
+			if(angle < 0)	( (angle < -90) ? (angle = angle + 360) : (angle = angle + 180) );
+			//rline..
+			if (((angle < 150) && (angle > 120)){
+				if (angle < langle) {
+					langle = angle;
+					lline = lines[i];
+				}
+			}
+			//lline..
+			if ((angle > 30) && (angle < 60))){
+				if (angle > rangle){
+					rangle = angle;
+					rline = lines[i];
+				}
+			}
 		}
 
-		imshow("edges", edges);
+		line(frame, lline, Scalar(0, 0, 255), 3, 8);
+		line(frame, rline, Scalar(0, 0, 255), 3, 8);
+
+//		imshow("edges", edges);
 		imshow("houghLine", frame);
 
 		if (waitKey(30) >= 0) break;
